6196b72b4e3bf874a6e394b58685a2d5cc6357a9
Michael Chadwick <michael.chadwick@gmail.com>
2014-07-30 00:59:35 -0700

    dynamic sound making works, but still working on IDs for play/stop buttons

--- js/audio.js
+++ js/audio.js
@@ -1,81 +1,142 @@
-var context = new (window.AudioContext || window.webkitAudioContext)();
-var audioBuffer = null;
+var soundNumber = 0;
 
-var AudioPlayer = {};
+// module for playing a sound
+var AudioPlayer = (function() {
+  // Variables
+  var soundId;
+  var audioContext;
+  var audioBuffer;
+  var gainNode;
+  var source;
+  var startOffset;
 
-AudioPlayer.gainNode = null;
-AudioPlayer.startOffset = 0;
-AudioPlayer.startTime = 0;
+  var soundDiv;
+  var soundHeader;
+  var fileUpload;
+  var rangeVolume;
+  var btnPlay;
+  var btnStop;
 
-AudioPlayer.play = function(startOffset) {
-  if(!context.createGain)
-    context.createGain = context.createGainNode;
-  this.gainNode = context.createGain();
+  // Constructor
+  var AudioPlayer = function() {
+    soundId = soundNumber;
+    console.log("soundId at construction", soundId);
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    audioBuffer = null;
+    gainNode = audioContext.createGain();
+    source = null;
+    startOffset = 0;
 
-  var source = context.createBufferSource();
-  source.buffer = audioBuffer;
+    soundDiv = document.createElement('div');
+    soundHeader = document.createElement('h3');
+    fileUpload = document.createElement('input');
+    rangeVolume = document.createElement('input');
+    btnPlay = document.createElement('button');
+    btnStop = document.createElement('button');
 
-  source.connect(this.gainNode);
-  this.gainNode.connect(context.destination);
-  source.loop = false;
+    soundDiv.classList.add('sound');
+    soundHeader.innerText = "Sound " + soundNumber;
 
-  source.start(0, this.startOffset);
-  this.source = source;
-}
+    fileUpload.id = "fileUpload" + soundNumber;
+    fileUpload.type = "file";
+    fileUpload.accept = "audio/*";
+    fileUpload.addEventListener('change', function(e) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        AudioPlayer.initSound(this.result, soundId);
+        console.log("soundId at initSound", soundId);
+      };
+      reader.readAsArrayBuffer(this.files[0]);
+    }, false);
 
-AudioPlayer.pause = function(curTime) {
-  this.startOffset = curTime;
-  this.source.stop();
-}
+    rangeVolume.id = "rangeVolume" + soundNumber;
+    rangeVolume.type = "range";
+    rangeVolume.min = 0;
+    rangeVolume.max = 100;
+    rangeVolume.value = 100;
+    rangeVolume.addEventListener('input', AudioPlayer.changeVolume(this));
 
-AudioPlayer.changeVolume = function(element) {
-  var volume = element.value;
-  var fraction = parseInt(element.value) / parseInt(element.max);
-  this.gainNode.gain.value = fraction * fraction;
-}
+    btnPlay.id = "btnPlay" + soundNumber;
+    btnPlay.innerText = "Play/Pause";
+    btnPlay.addEventListener('click', AudioPlayer.toggle);
+    btnPlay.disabled = true;
 
-AudioPlayer.stop = function() {
-  if (!this.source.stop) {
-    this.source.stop = source.noteOff;
+    btnStop.id = "btnStop" + soundNumber;
+    btnStop.innerText = "Stop";
+    btnStop.addEventListener('click', AudioPlayer.stop);
+    btnStop.disabled = true;
+
+    document.body.appendChild(soundDiv);
+    soundDiv.appendChild(soundHeader);
+    soundDiv.appendChild(fileUpload);
+    soundDiv.appendChild(rangeVolume);
+    soundDiv.appendChild(btnPlay);
+    soundDiv.appendChild(btnStop);
+
+    soundNumber++;
   }
-  this.source.stop(0);
-}
 
-AudioPlayer.toggle = function() {
-  this.playing ? this.pause(context.currentTime) : this.play(this.startOffset);
-  this.playing = !this.playing;
-}
+  // Methods
+  var getSoundId = function() {
+    return soundId;
+  }
 
-AudioPlayer.initSound = function(arrayBuffer) {
-  context.decodeAudioData(arrayBuffer, function(buffer) {
-    // audioBuffer is global to reuse the decoded audio later
-    audioBuffer = buffer;
-    var buttons = document.querySelectorAll('button');
-    buttons[0].disabled = false;
-    buttons[1].disabled = false;
-  }, function(e) {
-    console.log('Error decoding file', e);
-  });
-}
+  AudioPlayer.play = function(startOffset) {
+    if(!audioContext.createGain)
+      audioContext.createGain = audioContext.createGainNode;
+    gainNode = audioContext.createGain();
 
-// User selects file, read it as an ArrayBuffer and pass to the API
-var fileInput = document.querySelector('input[type="file"]');
-fileInput.addEventListener('change', function(e) {
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    AudioPlayer.initSound(this.result);
-  };
-  reader.readAsArrayBuffer(this.files[0]);
-}, false);
+    source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
 
-// Load file from a URL as an ArrayBuffer
-// Example: loading via xhr2: loadSoundFile('sounds/test.mp3');
-function loadSoundFile(url) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'arraybuffer';
-  xhr.onload = function(e) {
-    AudioPlayer.initSound(this.response); // this.response is an ArrayBuffer
-  };
-  xhr.send();
+    source.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    source.loop = false;
+
+    source.start(0, startOffset);
+  }
+
+  AudioPlayer.pause = function(curTime) {
+    startOffset = curTime;
+    source.stop();
+  }
+
+  AudioPlayer.changeVolume = function(element) {
+    var volume = element.value;
+    var fraction = parseInt(element.value) / parseInt(element.max);
+    gainNode.gain.value = fraction * fraction;
+  }
+
+  AudioPlayer.stop = function() {
+    if (!source.stop) {
+      source.stop = source.noteOff;
+    }
+    startOffset = 0;
+    source.stop(0);
+  }
+
+  AudioPlayer.toggle = function() {
+    this.playing ? AudioPlayer.pause(audioContext.currentTime) : AudioPlayer.play(startOffset);
+    this.playing = !this.playing;
+  }
+
+  AudioPlayer.initSound = function(arrayBuffer, sId) {
+    audioContext.decodeAudioData(arrayBuffer, function(buffer) {
+      audioBuffer = buffer;
+      console.log("btnPlay" + sId);
+      var btnP = document.getElementById("btnPlay" + sId);
+      btnP.disabled = false;
+      var btnS = document.getElementById("btnStop" + sId);
+      btnS.disabled = false;
+    }, function(e) {
+      console.log('Error decoding file', e);
+    });
+  }
+
+  return AudioPlayer;
+})();
+
+window.onload = function() {
+  var sound0 = new AudioPlayer();
+  var sound1 = new AudioPlayer();
 }
