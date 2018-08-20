abaeb2cea8dd12b9ce4e5f83501e358e452227b1
Michael Chadwick <mchadwick@ucsd.edu>
2018-08-20 01:32:04 -0700

    minor eslint adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "rules": {
         "indent": [
             "error",
-            4
+            2
         ],
         "linebreak-style": [
             "error",
@@ -20,6 +20,7 @@ module.exports = {
         "semi": [
             "error",
             "never"
-        ]
+        ],
+        "no-console": 0
     }
 };
